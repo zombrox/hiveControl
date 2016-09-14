452cd60f303039f98eb445c1c2cb68a6d22f84c7
Oleg Klimin <oleg.v.klimin@gmail.com>
2016-09-14 16:49:12 +0300

    First Release

--- proxyController/startProxy.sh
+++ proxyController/startProxy.sh
@@ -2,43 +2,43 @@
 proxyLocation=$1
 
 currentCoin=`cat currentCoin.txt | sed -e '/^#/ d'`
-echo $currentCoin
+#echo $currentCoin
 
 screenName=`echo $proxyLocation$currentCoin`
-echo $screenName
+#echo $screenName
 
 poolAddr=`cat currentPool.txt | sed -e '/^#/ d'| sed -e 's/\:/ /' | awk '{print $1}'`
-echo $poolAddr
+#echo $poolAddr
 
 poolPort=`cat currentPool.txt | sed -e '/^#/ d'| sed -e 's/\:/ /' | awk '{print $2}'`
-echo $poolPort
+#echo $poolPort
 
 case $currentCoin in
      lyra2re)
 let "stratumPort = $proxyLocation+49000"
-echo $stratumPort
+#echo $stratumPort
 
 let "getworkPort = $proxyLocation+50000"
-echo $getworkPort
+#echo $getworkPort
 ;;
      lyra2rev2)
 let "stratumPort = $proxyLocation+51000"
-echo $stratumPort
+#echo $stratumPort
 
 let "getworkPort = $proxyLocation+52000"
-echo $getworkPort
+#echo $getworkPort
 ;;
 
      Cryptonit)
 let "stratumPort = $proxyLocation+53000"
-echo $stratumPort
+#echo $stratumPort
 
 let "getworkPort = $proxyLocation+54000"
-echo $getworkPort
+#echo $getworkPort
 ;;
 
 esac
 
 
-#screen -dmS $screenName /scripts/quark-stratum-mining-proxy/mining_proxy.py --host $poolAddr --port $poolPort --stratum-port $stratumPort --getwork-port $getworkPort -q
-#echo `ps -ef | grep SCREEN | grep $screenName | awk '! /awk/ && /night/ {print $2}'` > '/tmp/'$screenName'Pid'
+screen -dmS $screenName /scripts/quark-stratum-mining-proxy/mining_proxy.py --host $poolAddr --port $poolPort --stratum-port $stratumPort --getwork-port $getworkPort -q
+echo `ps -ef | grep SCREEN | grep $screenName | awk '! /awk/ && /night/ {print $2}'` > '/tmp/'$screenName'Pid'
--- proxyController/stopProxy.sh
+++ proxyController/stopProxy.sh
@@ -2,10 +2,10 @@
 proxyLocation=$1
 
 currentCoin=`cat currentCoin.txt | sed -e '/^#/ d'`
-echo $currentCoin
+#echo $currentCoin
 
 screenName=`echo $proxyLocation$currentCoin`
-echo $screenName
+#echo $screenName
 
 
 kill `cat '/tmp/'$screenName'Pid'`
